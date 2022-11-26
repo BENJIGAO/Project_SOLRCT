3a5b0396087ac79d3afb2b374df69cf538b08a9a
Brian Gao <bhgao20014@gmail.com>
2022-11-25 19:40:35 -0500

    Test GitHub Actions workflow pt 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,47 @@
-import React, { useState, useEffect } from  'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import { getSchools } from './actions/schools';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
-import useStyles from './styles'
+import { getSchools } from "./actions/schools";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles";
 
 const App = () => {
+  console.log("This is to test the GitHub action workflow!");
   const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getSchools());
-  }, [dispatch])
+  }, [dispatch]);
   return (
     <Container maxWidth="md">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Schools</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">
+          Schools
+        </Typography>
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xs={8} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={4} sm={5}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
       </Grow>
     </Container>
-  )
-}
-
+  );
+};
 
 export default App;
